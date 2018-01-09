95d94f2e606365de35761fce533b85c361aec03a
Huy Hoang <huyhoang@Huys-MacBook-Pro.local>
2018-01-09 20:03:26 +0700

    update more models

--- gamma_demo_PyDBMS.py
+++ gamma_demo_PyDBMS.py
@@ -23,8 +23,8 @@ cur = connection.cursor()
 
 dataset = 'diabetes'
 dim = 9
-stm = 'SELECT DenseGamma(i, j, v USING PARAMETERS d=' + str(dim) + ') OVER (PARTITION BY MOD(i,1) ORDER BY i,j) FROM ' + dataset
 
+stm = 'SELECT DenseGamma(i, j, v USING PARAMETERS d=' + str(dim) + ') OVER (PARTITION BY MOD(i,1) ORDER BY i,j) FROM ' + dataset
 cur.execute(stm)
 rows = []
 cols = []
@@ -35,10 +35,27 @@ for row in cur.iterate():
     data.append(row[2])
 
 gamma = coo_matrix((np.asarray(data), (np.asarray(rows), np.asarray(cols))), shape=(dim+1, dim+1)).toarray()
+N = gamma[0,0]
 Q =  gamma[0:9, 0:9]
+L = gamma[1:9, 0]
 XYt =  gamma[0:9, 9]
 
-Q_inv = inv(Q)
+# Calculating Linear regression
 
+Q_inv = inv(Q)
 beta = np.dot(Q_inv, XYt)
 print beta
+
+# Calculating mean/variance
+mean = np.dot(1.0/N, L)
+variance = np.dot(1.0/N, Q) - np.dot(1.0/(N**2), np.dot(L, np.transpose(L)))
+std_dev = np.sqrt(variance)
+
+# Calculating correlation matrix
+rho_matrix = np.zeros(shape=(9, 9))
+for a in range(0, 9):
+    for b in range(0, 9):
+        rho_matrix[a, b] = (N*Q[a, b] - L[a]*L[b])/(sqrt(n*Q[a, a] - L[a]**2)*sqrt(n*Q[b, b] - L[b]**2))
+        rho_matrix[b, a] = rho_matrix[a, b] 
+
+
